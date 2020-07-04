29490f2e21e4f63bb1a0fa7ceec6288f2495183d
RAJ <vermar4142@gmail.com>
2020-07-04 21:49:10 +0530

    Added:- Calculating Parttime Employ Wage program file is added And Modified:- Modified UC1 and UC2 to enter name of employ

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -8,9 +8,11 @@ Attendence=$(( $RANDOM % 2 + 1 ))
 if [ $Attendence -eq 1 ]
 then
 	employ="Present"
+	read -p "Enter the Employ name : " name
 	echo $employ
 else
 	employ="Absent"
+	read -p "Enter the Employ name : " name
 	echo $employ
 fi
 
@@ -19,8 +21,21 @@ then
 	Employwageperhour=20
 	Empworkhour=8
 	dailyemploywage=$(($Employwageperhour * $Empworkhour))
-	echo $dailyemploywage
+	echo "Wage of $name who does full time is : "$dailyemploywage
 else
 	dailyemploywage=0
-	echo $dailyemploywage
+	echo "Employee Name $name was Absent today : "$dailyemploywage
+fi
+
+
+if [ "$employ" == "Present" ]
+then
+	Employwageperhour=20
+	Empworkhour=8
+	partime=$(($Empworkhour/2))
+	parttimeemploywage=$(($Employwageperhour * $partime))
+	echo "Wage of $name who does part time is : " $parttimeemploywage
+else
+	parttimeemploywage=0
+	echo "Employee Name $name was Absent today : "$dailyemploywage
 fi
