487ff0a669988edb52cc424ba5802491e93b5a9e
RAJ <vermar4142@gmail.com>
2020-07-04 20:59:01 +0530

    Added:- Check Employ Present Or Absent program file is added

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,3 +1,15 @@
 
 
 echo "Welcome to Employee Wage Computation Program on Master Branch "
+
+#!/bin/bash -x
+
+Attendence=$(( $RANDOM % 2 + 1 ))
+if [ $Attendence -eq 1 ]
+then
+	employ="Present"
+	echo $employ
+else
+	employ="Absent"
+	echo $employ
+fi
